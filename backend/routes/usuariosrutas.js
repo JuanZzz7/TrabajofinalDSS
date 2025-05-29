80021fac15807ec5f7e75b8ee8c8763875de371a
JuanZzz7 <dpena5803@gmail.com>
2025-05-29 09:06:23 -0500

    agrgacion en rutas

--- backend/routes/usuariosrutas.js
+++ backend/routes/usuariosrutas.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const usuariosmodel = require("../../backend/modelo/usuariosmodelo.js");
+const app = express();
 
 router.get("/",usuariosmodel.consultarDetalle);
 router.post("/",usuariosmodel.ingresar);
