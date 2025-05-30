aff0a0efe79c6e9e5ba093905a19244a7c9d337f
JuanZzz7 <dpena5803@gmail.com>
2025-05-30 13:30:59 -0500

    otra correccion

--- backend/modelo/usuariosmodelo.js
+++ backend/modelo/usuariosmodelo.js
@@ -50,7 +50,7 @@ class UsuariosController{
             //const docRef = await admin.firestore().collection('users').doc("user654").set(req.body);
             //Adicionando con nombre de documento asignado dinámicamente
             const docRef = await admin.firestore().collection('users').add(req.body);
-            //res.status(200).send ("Usuario agregado");
+            res.status(200).send ("Usuario agregado");
         }catch (err){
             res.status(500).send(err.message);
         }
