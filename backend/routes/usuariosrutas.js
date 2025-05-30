3e0db8adf334312d178c498041af8d4972abdc57
JuanZzz7 <dpena5803@gmail.com>
2025-05-30 12:33:03 -0500

    otro arreglo

--- backend/routes/usuariosrutas.js
+++ backend/routes/usuariosrutas.js
@@ -1,3 +1,4 @@
+/*
 const express = require("express");
 const router = express.Router();
 const usuariosmodel = require("../../backend/modelo/usuariosmodelo.js");
@@ -6,12 +7,32 @@ router.get("/",usuariosmodel.consultarDetalle);
 router.post("/",usuariosmodel.ingresar);
 
 /* Ejemplo con parámetros */
-
+/*
 router.route("/:iden")
 .get(usuariosmodel.consultarDetalle);
 /*
 .put(usuarioscontroller.actualizar)
 .delete(usuarioscontroller.borrar);
 */
+/*
+module.exports = router;
+*/
 
+const express = require("express");
+const router = express.Router();
+const usuariosmodel = require("../modelo/usuariosmodelo.js");
+ 
+router.get("/",usuariosmodel.consultarDetalle);
+router.post("/",usuariosmodel.ingresar);
+ 
+/* Ejemplo con parámetros */
+ 
+router.route("/:iden")
+.get(usuariosmodel.consultarDetalle)
+//.post(usuariosmodel.ingresar);
+/*
+.put(usuarioscontroller.actualizar)
+.delete(usuarioscontroller.borrar);
+*/
+ 
 module.exports = router;
